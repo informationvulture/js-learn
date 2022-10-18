897ebd84abc651e1cb07c66e19f4b9197a76ea2e
informationvulture <schoolemailnm1@gmail.com>
2022-10-17 21:46:27 -0700

    Fix needed for fib

--- dsa/objects/js/objects1.js
+++ dsa/objects/js/objects1.js
@@ -1,11 +1,35 @@
 // This is a new object
-let degrees = {};
+const degrees = {};
 
-degrees["BSc"] = "Bachelor of Science";
-degrees["BA"] = "Bachelor of Arts";
-degrees["MSc"] = "Master of Science";
-degrees["MA"] = "Master of Arts";
+degrees['BSc'] = 'Bachelor of Science';
+degrees['BA'] = 'Bachelor of Arts';
+degrees['MSc'] = 'Master of Science';
+degrees['MA'] = 'Master of Arts';
+degrees['PhD'] = 'Doctor of Philosophy';
+degrees['PhP'] = null;
 
-// Full output: {"BSc":"Bachelor of Science","BA":"Bachelor of Arts",
-// "MSc":"Master of Science","MA":"Master of Arts"}
-console.log(degrees); 
+console.log(degrees['PhP']);
+
+degrees['PhP'] = 'Doctor of PhP';
+
+console.log(degrees['PhP']);
+
+
+// You can do fibonacci using an object
+/**
+ *
+ * @param {Number} n - The number of fibonacci numbers to return
+ * @param {Object} computed - An object containing the computed fibonacci
+ * numbers
+ * @return {Number} The nth fibonacci number
+ */
+function fib(n, computed = {0: 0, 1: 1}) {
+  if (!(n in computed)) {
+    computed[n] = fib(n-1, computed) + fib(n-2, computed);
+  }
+  return computed[n];
+}
+
+function display() {
+  document.querySelector("p").innerText = fib(document.querySelector("input").value);
+}
