0cb439c710914b5e71685e51f43564469833c50e
informationvulture <schoolemailnm1@gmail.com>
2022-10-17 20:40:03 -0700

    Add objects; Improve arrays

--- dsa/arrays/js/arrays1.js
+++ dsa/arrays/js/arrays1.js
@@ -1,51 +1,63 @@
 // This is an array
-let arr1 = [1,2,3,4,5];
-document.getElementById("list").innerHTML = arr1;
+const arr1 = [1, 2, 3, 4, 5];
+document.getElementById('list').innerHTML = arr1;
 
-function do_push() {
-    // Push adds an element to the end of an array.
+/**
+ * @description This function adds an element to the end of an array.
+ */
+function doPush() {
+  // Push adds an element to the end of an array.
 
-    let value = arr1[arr1.length-1] + 1;
-    arr1.push(value);
-    document.getElementById("list").innerHTML = arr1;
-    document.getElementById("status").innerHTML = `Done push: ${value}`;
+  const value = arr1[arr1.length-1] + 1;
+  arr1.push(value);
+  document.getElementById('list').innerHTML = arr1;
+  document.getElementById('status').innerHTML = `Done push: ${value}`;
 }
 
-function do_unshift() {
-    // Unshift adds an element to the start of an array.
+/**
+ * @Description This function adds an element to the beginning of an array.
+ */
+function doUnshift() {
+  // Unshift adds an element to the start of an array.
 
-    let value = arr1[0] - 1;
-    arr1.unshift(value);
-    document.getElementById("list").innerHTML = arr1;
-    document.getElementById("status").innerHTML = `Done unshift: ${value}`;
+  const value = arr1[0] - 1;
+  arr1.unshift(value);
+  document.getElementById('list').innerHTML = arr1;
+  document.getElementById('status').innerHTML = `Done unshift: ${value}`;
 }
 
-function do_pop() {
-    // Pop removes the last element of an array
-    // and returns it.
+/**
+ * @description This function removes the last element of an array
+ */
+function doPop() {
+  // Pop removes the last element of an array
+  // and returns it.
 
-    val = arr1.pop();
-    document.getElementById("list").innerHTML = arr1;
-    document.getElementById("status").innerHTML = `Done pop: ${val}`;
+  val = arr1.pop();
+  document.getElementById('list').innerHTML = arr1;
+  document.getElementById('status').innerHTML = `Done pop: ${val}`;
 }
 
-function do_shift() {
-    // Shift removes the first element of an array
-    // and returns it.
+/**
+ * @description This function removes the first element of an array
+ */
+function doShift() {
+  // Shift removes the first element of an array
+  // and returns it.
 
-    let val = arr1.shift();
-    document.getElementById("list").innerHTML = arr1;
-    document.getElementById("status").innerHTML = `Done shift: ${val}`;
+  const val = arr1.shift();
+  document.getElementById('list').innerHTML = arr1;
+  document.getElementById('status').innerHTML = `Done shift: ${val}`;
 }
 
-const el = document.getElementById("pop");
-el.addEventListener("click", do_pop, false);
+const el = document.getElementById('pop');
+el.addEventListener('click', doPop, false);
 
-const el2 = document.getElementById("shift");
-el2.addEventListener("click", do_shift, false);
+const el2 = document.getElementById('shift');
+el2.addEventListener('click', doShift, false);
 
-const el3 = document.getElementById("push");
-el3.addEventListener("click", do_push, false);
+const el3 = document.getElementById('push');
+el3.addEventListener('click', doPush, false);
 
-const el4 = document.getElementById("unshift");
-el4.addEventListener("click", do_unshift, false);
+const el4 = document.getElementById('unshift');
+el4.addEventListener('click', doUnshift, false);
---
+++ dsa/objects/js/objects1.js
@@ -0,0 +1,11 @@
+// This is a new object
+let degrees = {};
+
+degrees["BSc"] = "Bachelor of Science";
+degrees["BA"] = "Bachelor of Arts";
+degrees["MSc"] = "Master of Science";
+degrees["MA"] = "Master of Arts";
+
+// Full output: {"BSc":"Bachelor of Science","BA":"Bachelor of Arts",
+// "MSc":"Master of Science","MA":"Master of Arts"}
+console.log(degrees); 
