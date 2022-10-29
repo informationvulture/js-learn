c5129112f274fda05e192522ae4913fa351bd987
informationvulture <schoolemailnm1@gmail.com>
2022-10-28 21:45:31 -0700

    Add start of Books project

---
+++ PROJECTS/bookreview/js/bookreview.js
@@ -0,0 +1,12 @@
+// Import the bookVault array from books.js
+import { bookVault } from "./books.js";
+
+
+
+// See data
+
+for (let i = 0; i < bookVault.length; i++) {
+    for (let book in bookVault[i]) {
+        console.log(bookVault[i][book]);
+    }
+}
---
+++ PROJECTS/bookreview/js/books.js
@@ -0,0 +1,12 @@
+let bookVault = [
+    {
+        title: "The Sovereign Individual",
+        author: ["James Dale Davidson", "Lord William Rees-Mogg"],
+        year: 1997,
+        currentPage: 52
+
+    }
+];
+
+// Export the bookVault array
+export { bookVault };
